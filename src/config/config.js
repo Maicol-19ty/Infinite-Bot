4bd958d5ee7878e939c18333245e23cef869c915
Maicol <maicolmontoya323@gmail.com>
2024-01-18 16:34:32 -0500

    Update dependencies in package.json

---
+++ src/config/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    server: {
+        domain: "infinitehorizon.me",
+        port: 8080,
+        https: false
+    },
+
+    Discord: {
+        token: "MTE5NzQwOTc2NzY2NTE4ODg4NA.GbLqHa.Xc7IMptgU-W_UIGt-ac75ELo5BqxMXQkkGPmCY",
+        botId: "1197409767665188884",
+        guildId: "",
+        verifiedRole: "",
+
+        removeRole: false,
+        removeRoleId: "",
+
+        statusType: 3,
+        statusMsg: "unverified users!",
+
+        rulesEnabled: true,
+        rules: "Type your rules here if rulesEnabled is enabled, ensure to use \n for new lines",
+    },
+
+    reCaptcha: {
+        siteKey: '6LcyglUpAAAAAGgO5CFwupif1KUD2650hDk7cAoY',
+        secretKey: '6LcyglUpAAAAAExF6irwHIwOVZ-DlXFZsJNzNLW2',
+    },
+}
